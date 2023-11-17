c4524b90a29f295803c868f8672b3f74ce1b3bb5
hritviik715 <hritvikmohan7151221@gmail.com>
2023-11-18 00:25:48 +0530

    last_commit_of_today
    fuckoff

--- z.cpp
+++ z.cpp
@@ -5,14 +5,14 @@ int main(){
 
 
 
-// for(int i=0 ; i<3 ; i++){
-//     int x=0;
-//     for(int j=0; j<3 ; j++){
-//         x=x+2;
-//         cout<<x<<" ";
-//     }
-//     cout<<endl;
-// }
+for(int i=0 ; i<3 ; i++){
+    int x=0;
+    for(int j=0; j<3 ; j++){
+        x=x+2;
+        cout<<x<<" ";
+    }
+    cout<<endl;
+}
 
 
 

93ae0f8d02494141b80bc9efdf6bce2eb910e432
hritviik715 <hritvikmohan7151221@gmail.com>
2023-11-18 13:40:43 +0530

    a

--- function/pnc.cpp
+++ function/pnc.cpp
@@ -11,8 +11,34 @@ cout<<"enter r :";
 cin>>r;
 
 
+int a = 1;
+for(int i=1; i<=n  ; i++){
+    a=a*i;
+}
+
+cout<<a<<endl;
 
 
+
+
+int b=1;
+for(int i=1; i<=r ; i++){
+    b=b*i;
+}
+
+cout<<b<<endl;
+
+
+int c=1;
+for(int i=1; i<=(n-r) ; i++){
+    c=c*i;
+}
+
+cout<<c<<endl;
+
+
+cout<<"permutation is : "<<a/c<<endl  ;
+cout<<"combination is : "<<a/(b*c)<<endl;
 
 
 

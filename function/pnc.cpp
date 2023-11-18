f55cf00940be10c1a124af65b6b64ac2d18a46f7
hritviik715 <hritvikmohan7151221@gmail.com>
2023-11-18 14:54:06 +0530

    s

--- function/pnc.cpp
+++ function/pnc.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
 using namespace std;
+
+int fact(int x)
+{
+    int f=1;
+    for(int i= 1; i<=x ; i++)
+    {
+        f=f*i;
+    }
+    return f;
+}
+
+
 int main(){
-
-
 int n;
 cout<<"enter n : ";
 cin>>n;
@@ -10,42 +20,58 @@ int r ;
 cout<<"enter r :";
 cin>>r;
 
-
-int a = 1;
-for(int i=1; i<=n  ; i++){
-    a=a*i;
-}
-
-cout<<a<<endl;
+// long way 
+// long way 
+// long way 
 
 
+// int a = 1;
+// for(int i=1; i<=n  ; i++){
+//     a=a*i;
+// }
+
+// cout<<a<<endl;
 
 
-int b=1;
-for(int i=1; i<=r ; i++){
-    b=b*i;
-}
-
-cout<<b<<endl;
 
 
-int c=1;
-for(int i=1; i<=(n-r) ; i++){
-    c=c*i;
-}
+// int b=1;
+// for(int i=1; i<=r ; i++){
+//     b=b*i;
+// }
 
-cout<<c<<endl;
+// cout<<b<<endl;
 
 
-cout<<"permutation is : "<<a/c<<endl  ;
+// int c=1;
+// for(int i=1; i<=(n-r) ; i++){
+//     c=c*i;
+// }
+
+// cout<<c<<endl;
+
+
+// cout<<"permutation is : "<<a/c<<endl  ;
+// cout<<"combination is : "<<a/(b*c)<<endl;
+
+
+
+
+
+// shoert way 
+// shoert way 
+// shoert way 
+
+
+int a=fact(n);
+int b=fact(r);
+int c=fact(n-r);
+
+
+
+
+cout<<"permutation is : "<<a/c<<endl ;
 cout<<"combination is : "<<a/(b*c)<<endl;
-
-
-
-
-
-
-
 
 
 

1e77f50c420f54ded2d140588d353cd6fe4b6ad4
hritviik715 <hritvikmohan7151221@gmail.com>
2023-11-18 12:31:00 +0530

    a

---
+++ function/basics.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void greet(){
+    cout<<endl;
+    cout<<"good morning"<<endl;
+    cout<<"hitvik"<<endl;
+    cout<<endl;
+
+
+}
+
+
+int main(){
+
+    greet();
+
+
+}
--- pattern_printing/floyds_triangle.cpp
+++ pattern_printing/floyds_triangle.cpp
@@ -6,7 +6,7 @@ int n;
 cout<<"enter n : ";
 cin>>n;
 
-
+//it is pendiong
 
 for(int i=1; i<=n ; i++){
     for(int j=i ; )

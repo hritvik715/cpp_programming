b24214a280b201feb4eaf242c50eac3014b8ea5f
hritviik715 <hritvikmohan7151221@gmail.com>
2023-11-24 00:01:25 +0530

    X

--- recursion/fibo.cpp
+++ recursion/fibo.cpp
@@ -1,14 +1,15 @@
 #include<iostream>
 using namespace std;
 
-int fibo(int a ,int b){
-    if(b==0) return 1;
-    return a*power(a,b-1);
+int fibo(int a){
+    if(a==1) return 0;
+    if(a==2 || a==2) return 1;
+    return fibo(a-1) + fibo(a-2);   //NOTE--> yha else lga bhi sakte ho ya nhi bhi
 }
 
 
 int main(){
-    cout<<power(2,3);
+    cout<<fibo(1);
 
 
 

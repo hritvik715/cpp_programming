a2b130ac2f1ee973a9036d5e6d84e3cb0b9ef6a7
hritviik715 <hritvikmohan7151221@gmail.com>
2023-11-24 01:57:24 +0530

    s

--- array/max_element_in_array.cpp
+++ array/max_element_in_array.cpp
@@ -1,16 +1,24 @@
 #include<iostream>
+#include<stdio.h>
 using namespace std;
 
 
 int main(){
     int arr[] = {5,3,2};
-    int max;
-    for(int i=0 ; i < sizeof(arr)/4 ; i++){
-        for(int j=i+1 ; j<sizeof(arr)/4 ; j++){
-            if(arr[i] > arr[j]) max = arr[i];
-        }
+    int mx=arr[0];
+    int n = sizeof(arr)/4;
+    for(int i = 0 ; i<n ;i++){
+        // first way
+        // if(arr[i]>mx) mx =arr[i]; 
+          
+        //second way
+        mx = max(mx,arr[i]);
     }
-    
+    cout<<mx;
+
+
+
+
 }
 
 //we conslude that array --> pass by refrence hote hai

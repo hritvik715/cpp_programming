6dbd68c9fd1a9e764125c611cddde088f4d9a99f
hritviik715 <hritvikmohan7151221@gmail.com>
2023-11-24 23:01:40 +0530

    c

--- z.cpp
+++ z.cpp
@@ -5,16 +5,18 @@ int main(){
 
 
 
-for(int i=0 ; i<3 ; i++){
-    int x=0;
-    for(int j=0; j<3 ; j++){
-        x=x+2;
-        cout<<x<<" ";
-    }
-    cout<<endl;
-}
+// for(int i=0 ; i<3 ; i++){
+//     int x=0;
+//     for(int j=0; j<3 ; j++){
+//         x=x+2;
+//         cout<<x<<" ";
+//     }
+//     cout<<endl;
+// }
 
-
+int x=65;
+char y = (int)x;
+cout<<y;
 
 
 }
